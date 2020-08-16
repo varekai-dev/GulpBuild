8babcb1d179ede9460ec7ff60ee8eba74f7db29f
Serhii <serhijsav@gmail.com>
2020-08-16 23:58:47 +0300

    add sticky footer, normalize css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-let project_folder = require("path").basename(__dirname)
+let project_folder = "dist"
+// let project_folder = require("path").basename(__dirname)  name of folder dist
 let source_folder = "#src"
 let fs = require("fs")
 
