1e60e99f7142b352058c00e178e46c9c8f5520cd
varekaiwork <serhiisaviukwork@gmail.com>
2020-11-13 16:59:16 +0200

    revert

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,36 +1,16 @@
-
-document.addEventListener('DOMContentLoaded', function(){ 
-  const open = document.querySelector("#open_popup")
-  const popupBg = document.querySelector('.popup-bg')
-  const close = document.querySelector('#close')
-  const content =document.querySelector('.content')
-
- 
-
-
-  function closed(){
-    popupBg.classList.remove("visible")
-    content.classList.remove('overflow')
+function testWebP(callback) {
+  var webP = new Image()
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2)
   }
-  open.addEventListener('click', function(){
-    popupBg.classList.add("visible")
-    content.classList.add('overflow')
-   
-  })
-  close.addEventListener('click', closed)
-  popupBg.addEventListener('click', (e)=>{
-    if(e.target===popupBg || e.keyCode === 27 ){
-      closed()
-    }
-  
-  })
-  
-  document.addEventListener('keydown',(e)=>{
-    if(e.code ==="Escape"){
-      closed()
-    }
-  })
-  
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp")
+  } else {
+    document.querySelector("body").classList.add("no-webp")
+  }
 })
-
-
